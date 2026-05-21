606ba30e1f920c29537cc3707a977a5bba99bb5a
Johnny_Ma <johnny880319@gmail.com>
2026-05-21 22:23:34 +0800

    codecrafters submit [skip ci]

--- internal/database/replication.go
+++ internal/database/replication.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"context"
 	"errors"
+	"fmt"
+	"io"
 	"net"
+	"strconv"
 )
 
 // RunReplication connects to the master and starts the replication process.
@@ -35,5 +38,44 @@ func (db *Database) RunReplication(masterAddr string) (err error) {
 			return err
 		}
 	}
+	err = readRDB(reader)
+	if err != nil {
+		return err
+	}
+
+	client := &client{db: db, conn: conn, watched: make(map[string]string)}
+
+	for {
+		command, _, err := readCommand(reader)
+		if err != nil {
+			return fmt.Errorf("error reading command: %w", err)
+		}
+		if len(command) == 0 {
+			continue
+		}
+
+		_ = client.handleCommand(command)
+	}
+}
+
+func readRDB(reader *bufio.Reader) error {
+	line, err := readRespLine(reader)
+	if err != nil {
+		return err
+	}
+	if len(line) == 0 || line[0] != '$' {
+		return fmt.Errorf("invalid RDB format")
+	}
+
+	rdbLen, err := strconv.Atoi(line[1:])
+	if err != nil || rdbLen < 0 {
+		return fmt.Errorf("invalid RDB length: %w", err)
+	}
+
+	buf := make([]byte, rdbLen)
+	if _, err := io.ReadFull(reader, buf); err != nil {
+		return fmt.Errorf("error reading RDB data: %w", err)
+	}
+
 	return nil
 }
