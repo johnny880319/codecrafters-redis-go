712e5097fa9751faa5d197f2cba99c4c8f119743
Johnny_Ma <johnny880319@gmail.com>
2026-06-09 21:41:17 +0800

    codecrafters submit [skip ci]

--- internal/database/cmd_authentication.go
+++ internal/database/cmd_authentication.go
@@ -3,13 +3,15 @@ package database
 import "strings"
 
 func (c *client) cmdAcl(args []string) []byte {
-	if len(args) != 1 {
-		return simpleError("Usage: ACL <subcommand>")
+	if len(args) < 1 {
+		return simpleError("ERR wrong number of arguments for 'ACL' command")
 	}
 
 	switch strings.ToUpper(args[0]) {
 	case "WHOAMI":
 		return bulkString("default", true)
+	case "GETUSER":
+		return respArray([][]byte{bulkString("flags", true), respArray([][]byte{})})
 	default:
 		return simpleError("ERR unknown ACL subcommand")
 	}
